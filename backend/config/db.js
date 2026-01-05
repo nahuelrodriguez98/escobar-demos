b3e16d749e7ce165d66e34d16fd79416f5864cc3
Nahuel Rodriguez <nahuelrodriguez_@outlook.com>
2026-01-05 13:56:38 -0300

    asda

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,45 +1,4 @@
-﻿/*require('dotenv').config(); 
-const sql = require('pg');
-
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
-  options: { encrypt: (process.env.DB_ENCRYPT === 'true') }
-};
-
-let pool = null;
-
-async function getPool() {
-  if (pool) return pool;
-  pool = await sql.connect(config);
-  return pool;
-}
-
-async function query(queryStr, params = {}) {
-  const p = await getPool();
-  const req = p.request();
-
-  for (const k of Object.keys(params)) {
-    const value = params[k];
-
-    let type;
-    if (typeof value === "number") type = sql.Int;
-    else if (typeof value === "string") type = sql.VarChar;
-    else if (value instanceof Date) type = sql.DateTime;
-    else type = sql.VarChar;
-
-    req.input(k, type, value);
-  }
-
-  const res = await req.query(queryStr);
-  return res;
-}
-
-module.exports = { query, sql };
-*/
-require("dotenv").config();
+﻿require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -51,7 +10,7 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 5000,
 });
 
 async function query(text, params = []) {
